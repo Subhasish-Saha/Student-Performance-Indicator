d997d565fc1f4531f565619706564505bf72224b
Subhasish Saha <subhasishsaha007@gmail.com>
2023-09-06 22:06:41 +0530

    Added : Model Evaluator

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import pickle
 import numpy as np
 import pandas as pd
 
+from sklearn.metrics import r2_score
 from src.exception import Custom_Exception
 
 def save_object(file_path:str, obj:object) -> None:
@@ -18,3 +19,24 @@ def save_object(file_path:str, obj:object) -> None:
     
     except Exception as e:
         raise Custom_Exception(e,sys)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise Custom_Exception(e, sys)
